656a5790206ea1846de212e4555230f8e2de3237
Unknown <Mahe.spaenle@etu.umontpellier.fr>
2017-05-13 15:26:22 +0200

    add navbar
    Corrected Paths

--- View/index.php
+++ View/index.php
@@ -1,1 +1,16 @@
-Hello World!
+<!DOCTYPE html>
+<html ng-app='videoGames'>
+  <head>
+    <meta charset="utf-8" />
+    <link rel="stylesheet" type="text/css" href="/Website/View/bootstrap-3.3.7-dist/css/bootstrap.min.css" />
+    <link rel="stylesheet" href="/Website/View/css/custom.css" />
+    <script type="text/javascript" src="/Website/View/angular.min.js"></script>
+    <script type="text/javascript" src="/Website/View/app.js"></script>
+  </head>
+  <body>
+    <?php include("nav.php"); ?>
+    <div class="jumbotron text-center">
+      <h1 class ="display-1">{{ "Wanna play a game ?"}}</h1>
+    </div>
+  </body>
+</html>
