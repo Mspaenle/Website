cb82b3100faefb7cb940a79edbe8cf4d5ad17d60
Mahé <Mahe.spaenle@etu.umontpellier.fr>
2017-05-16 14:47:54 +0200

    edit path

--- Controller/controller_page_index.php
+++ Controller/controller_page_index.php
@@ -1,5 +1,5 @@
 <?php
 
-	require_once("/Website/View/index.php");
+	require_once("View/index.php");
 
 ?>
--- View/index.php
+++ View/index.php
@@ -2,11 +2,11 @@
 <html ng-app='videoGames'>
   <head>
     <meta charset="utf-8" />
-    <link rel="stylesheet" type="text/css" href="/Website/View/bootstrap-3.3.7-dist/css/bootstrap.min.css" />
-    <link rel="stylesheet" type="text/css" href="/Website/View/bootstrap-3.3.7-dist/css/heroic-features.css" />
-    <script type="text/javascript" src="/Website/View/angular.min.js"></script>
-    <script type="text/javascript" src="/Website/View/app.js"></script>
-    <script src="/Website/Controller/gamesController.js"></script>
+    <link rel="stylesheet" type="text/css" href="View/bootstrap-3.3.7-dist/css/bootstrap.min.css" />
+    <link rel="stylesheet" type="text/css" href="View/bootstrap-3.3.7-dist/css/heroic-features.css" />
+    <script type="text/javascript" src="View/angular.min.js"></script>
+    <script type="text/javascript" src="View/app.js"></script>
+    <script src="Controller/gamesController.js"></script>
     <title> Home </title>
   </head>
 
