e1a21b2b2af4eb7ead8583341c803a2e9a33ba25
Mahé <Mahe.spaenle@etu.umontpellier.fr>
2017-05-16 22:24:55 +0200

    rank1

--- View/ranking.php
+++ View/ranking.php
@@ -1,39 +1,65 @@
 <!DOCTYPE html>
+
 <html ng-app='Games'>
+
   <head>
     <meta charset="utf-8" />
     <link rel="stylesheet" type="text/css" href="View/bootstrap-3.3.7-dist/css/bootstrap.min.css" />
     <link rel="stylesheet" type="text/css" href="View/relic/css/heroic-features.css" />
-
-	<!-- Themify Icons -->
-	<link rel="stylesheet" href="View/relic/css/themify-icons.css">
-	<!-- FontAwesome Icons -->
-	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.3.0/css/font-awesome.min.css">
-	<!-- Bootstrap -->
-	<link rel="stylesheet" href="View/relic/css/bootstrap.min.css">
-	<!-- Owl Carousel -->
-	<link rel="stylesheet" href="View/relic/css/owl.carousel.min.css">
-	<link rel="stylesheet" href="View/relic/css/owl.theme.default.min.css">
-	<!-- Magnific Popup -->
-	<link rel="stylesheet" href="View/relic/css/magnific-popup.css">
-	<!-- Easy Responsive Tabs -->
-	<link rel="stylesheet" href="View/relic/css/easy-responsive-tabs.css">
-	<!-- Theme Style -->
-	<link rel="stylesheet" href="View/relic/css/style.css">
-
+  	<!-- Themify Icons -->
+  	<link rel="stylesheet" href="View/relic/css/themify-icons.css">
+  	<!-- FontAwesome Icons -->
+  	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.3.0/css/font-awesome.min.css">
+  	<!-- Bootstrap -->
+  	<link rel="stylesheet" href="View/relic/css/bootstrap.min.css">
+  	<!-- Owl Carousel -->
+  	<link rel="stylesheet" href="View/relic/css/owl.carousel.min.css">
+  	<link rel="stylesheet" href="View/relic/css/owl.theme.default.min.css">
+  	<!-- Magnific Popup -->
+  	<link rel="stylesheet" href="View/relic/css/magnific-popup.css">
+  	<!-- Easy Responsive Tabs -->
+  	<link rel="stylesheet" href="View/relic/css/easy-responsive-tabs.css">
+  	<!-- Theme Style -->
+  	<link rel="stylesheet" href="View/relic/css/style.css">
     <script type="text/javascript" src="View/angular.min.js"></script>
     <script type="text/javascript" src="Controller/gamesController.js"></script>
     <title> Ranking </title>
   </head>
+
   <body>
+
     <?php include("nav.php"); ?>
+
     <div class="page-header">
       <h1 class ="display-3 text-center">{{ "Ranking"}}</h1>
     </div>
+
+    <div id="js-fh5co-slider">
+      <div class="container">
+        <div class="fh5co-section-heading">
+          <h2 class="text-muted">Our Cool Office</h2>
+          <h3 class="text-center">Visit us and have a cup of coffee</h3>
+        </div>
+
+        <div class="row">
+          <div class="col-md-12">
+            <div class="owl-carousel owl-carousel-fullwidth">
+                <div class="item"><img src="images/img_large_1.jpg" alt="image"></div>
+                <div class="item"><img src="images/img_large_2.jpg" alt="image"></div>
+                <div class="item"><img src="images/img_large_3.jpg" alt="image"></div>
+                <div class="item"><img src="images/img_large_4.jpg" alt="image"></div>
+                <div class="item"><img src="images/img_large_5.jpg" alt="image"></div>
+                <div class="item"><img src="images/img_large_6.jpg" alt="image"></div>
+                <div class="item"><img src="images/img_large_7.jpg" alt="image"></div>
+
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+
     <div class="container">
-
       <?php include("foot.php")?>
-
     </div>
 
 
