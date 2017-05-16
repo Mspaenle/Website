6718d62396fcf7f2f5a139149f3dfa610d53033f
Mahé <Mahe.spaenle@etu.umontpellier.fr>
2017-05-16 22:02:44 +0200

    Ok

--- View/nav.php
+++ View/nav.php
@@ -1,6 +1,6 @@
 <nav class="navbar navbar-inverse navbar-fixed-top" role="navigation" id="navigationBar">
-    <div class="container">
-        <div class="navbar-header">
+  <div class="fh5co-nav-overlay"></div>
+  <div class="fh5co-nav-inner">
             <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="navbarColl" href="index.php">
                 <span class="sr-only">Toggle navigation</span>
                 <span class="icon-bar"></span>
@@ -10,7 +10,7 @@
             </button>
         </div>
         <div class="collapse navbar-collapse" id="navbarColl">
-            <ul id="fh5co-main-nav">
+            <ul class="nav navbar-nav">
               <li><a href="index.php">Home</a></li>
               <li><a href="games.php">Video Games</a></li>
               <li><a href="ranking.php">Ranking</a></li>
