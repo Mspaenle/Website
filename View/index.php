eefb3f3ff2581fb0fa6d31f8e3400806c5da429f
Mahé <Mahe.spaenle@etu.umontpellier.fr>
2017-05-16 20:09:20 +0200

    Byebye

--- View/index.php
+++ View/index.php
@@ -4,9 +4,9 @@
     <meta charset="utf-8" />
     <link rel="stylesheet" type="text/css" href="View/bootstrap-3.3.7-dist/css/bootstrap.min.css" />
     <link rel="stylesheet" type="text/css" href="View/bootstrap-3.3.7-dist/css/heroic-features.css" />
-    <script src="View/angular.min.js"></script>
+    <script type="text/javascript" src="View/angular.min.js"></script>
     <script src="Controller/gamesController.js"></script>
-    <title> Moy Games </title>
+    <title> Moys Games </title>
   </head>
 
     <body class="list-group" ng-controller="GamesController as game">
