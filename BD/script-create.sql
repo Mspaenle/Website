e26deff8c8173cc0948947db9cb77e4e27b82453
Mahé <Mahe.spaenle@etu.umontpellier.fr>
2017-05-18 14:59:19 +0200

    Scripts BD

---
+++ BD/script-create.sql
@@ -0,0 +1,70 @@
+﻿CREATE TABLE admin
+(
+    idadmin numeric(6) NOT NULL,
+    mdp character varying(20),
+    CONSTRAINT pk_admin PRIMARY KEY (idadmin)
+);
+
+CREATE TABLE game
+(
+    idgame numeric(6) NOT NULL,
+    namegame character varying(20) ,
+    description character varying(250) ,
+    image character varying(250) ,
+    CONSTRAINT pk_game PRIMARY KEY (idgame)
+);
+
+CREATE TABLE party
+(
+    idparty numeric(6) NOT NULL,
+    nameparty character varying(20),
+    idgame numeric(6),
+    CONSTRAINT pk_party PRIMARY KEY (idparty),
+    CONSTRAINT fk_party FOREIGN KEY (idgame)
+        REFERENCES game(idgame)
+);
+
+CREATE TABLE player
+(
+    idplayer numeric(6) NOT NULL,
+    name character varying(20),
+    score numeric(6),
+    CONSTRAINT pk_player PRIMARY KEY (idplayer)
+);
+
+CREATE TABLE team
+(
+    idteam numeric(6) NOT NULL,
+    teamname character varying(20) NOT NULL,
+    CONSTRAINT pk_team PRIMARY KEY (idteam),
+    CONSTRAINT team_teamname_key UNIQUE (teamname)
+);
+
+
+CREATE TABLE participate
+(
+    idplayer numeric(6) NOT NULL,
+    idparty numeric(6) NOT NULL,
+    scoreparty numeric(6),
+    CONSTRAINT pk_participate PRIMARY KEY (idplayer, idparty),
+    CONSTRAINT fk_participate1 FOREIGN KEY (idplayer)
+        REFERENCES player (idplayer),
+    CONSTRAINT fk_participate2 FOREIGN KEY (idparty)
+        REFERENCES party (idparty)
+);
+
+CREATE TABLE participateteam
+(
+    idplayer numeric(6) NOT NULL,
+    idparty numeric(6) NOT NULL,
+    idteam numeric(6),
+    scoreparty numeric(6),
+    CONSTRAINT pk_participateteam PRIMARY KEY (idplayer, idparty),
+    CONSTRAINT fk_participateteam1 FOREIGN KEY (idplayer)
+        REFERENCES player (idplayer),
+    CONSTRAINT fk_participateteam2 FOREIGN KEY (idparty)
+        REFERENCES party (idparty),
+    CONSTRAINT fk_participateteam3 FOREIGN KEY (idteam)
+        REFERENCES team (idteam)
+);
+
---
+++ BD/script-insert.sql
@@ -0,0 +1,29 @@
+﻿INSERT INTO admin VALUES (199612,97419);
+INSERT INTO game VALUES(1,'Fifa17','FIFA17 is a football video game. You have the possibility to play by teams of two.','/images/Fifa17.jpg');
+INSERT INTO game VALUES(2,'Mario Party','Mario Party is a family game you can play in teams','images/MarioParty.jpg');
+INSERT INTO game VALUES(3,'Mario Kart','Mario Kart is a racing famlily game.','images/MarioKart.jpg');
+INSERT INTO player VALUES(1,'Lapinou13',10);
+INSERT INTO player VALUES(2,'Cassius98',13);
+INSERT INTO player VALUES(3,'Hera12',3);
+INSERT INTO player VALUES(4,'Kavinouu',2);
+INSERT INTO player VALUES(5,'Helene',10);
+INSERT INTO player VALUES(6,'Ecko',12);
+INSERT INTO player VALUES(7,'Bunny',9);
+INSERT INTO player VALUES(8,'Ohlyy',3);
+INSERT INTO player VALUES(9,'Radjh',12);
+INSERT INTO player VALUES(10,'Marcus',2);
+INSERT INTO team VALUES(1,11);
+INSERT INTO team VALUES(2,12);
+INSERT INTO party VALUES(1,'Fifa Raclette',1);
+INSERT INTO party VALUES(2,'LouBd',2);
+INSERT INTO party VALUES(3,'Race Hard',3);
+INSERT INTO participate VALUES(2,2,25);
+INSERT INTO participate VALUES(5,3,15);
+INSERT INTO participate VALUES(7,2,12);
+INSERT INTO participate VALUES(10,3,20);
+INSERT INTO participateteam VALUES(1,1,1,3);
+INSERT INTO participateteam VALUES(3,1,2,2);
+INSERT INTO participateteam VALUES(8,1,2,2);
+INSERT INTO participateteam VALUES(9,1,1,3);
+
+
