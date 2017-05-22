17907651fb00fcf26622f267141b6b895e72403f
Mahé <Mahe.spaenle@etu.umontpellier.fr>
2017-05-22 19:15:27 +0200

    BD

--- BD/script-create.sql
+++ BD/script-create.sql
@@ -1,24 +1,26 @@
-﻿CREATE TABLE admin
+﻿CREATE TABLE administrator
 (
     idadmin numeric(6) NOT NULL,
-    mdp character varying(20),
+    mdp varchar(120),
     CONSTRAINT pk_admin PRIMARY KEY (idadmin)
 );
 
 CREATE TABLE game
 (
     idgame SERIAL NOT NULL,
-    namegame character varying(20) ,
-    description character varying(250) ,
-    image character varying(250) ,
+    namegame varchar(20) ,
+    description varchar(250) ,
+    image varchar(250) ,
     CONSTRAINT pk_game PRIMARY KEY (idgame)
 );
 
 CREATE TABLE party
 (
     idparty SERIAL NOT NULL,
-    nameparty character varying(20),
+    nameparty varchar(20),
     idgame integer,
+    team integer,
+    nbplayer integer DEFAULT 1,
     CONSTRAINT pk_party PRIMARY KEY (idparty),
     CONSTRAINT fk_party FOREIGN KEY (idgame)
         REFERENCES game(idgame)
@@ -27,7 +29,7 @@ CREATE TABLE party
 CREATE TABLE player
 (
     idplayer SERIAL NOT NULL,
-    name character varying(20),
+    name varchar(20),
     score numeric(6),
     CONSTRAINT pk_player PRIMARY KEY (idplayer)
 );
@@ -35,7 +37,7 @@ CREATE TABLE player
 CREATE TABLE team
 (
     idteam SERIAL NOT NULL,
-    teamname character varying(20) NOT NULL,
+    teamname varchar(20) NOT NULL,
     CONSTRAINT pk_team PRIMARY KEY (idteam),
     CONSTRAINT team_teamname_key UNIQUE (teamname)
 );
@@ -67,4 +69,3 @@ CREATE TABLE participateteam
     CONSTRAINT fk_participateteam3 FOREIGN KEY (idteam)
         REFERENCES team (idteam)
 );
-
