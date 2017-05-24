595af15cb2f2cf9285af49e92b9fc3e5e9b4903b
Unknown <Mahe.spaenle@etu.umontpellier.fr>
2017-05-24 12:41:16 +0200

    Test

--- index.php
+++ index.php
@@ -2,7 +2,7 @@
 <head>
 <title>Redirection en HTML</title>
 
-<meta http-equiv="refresh" content="5; URL="home"">
+<meta http-equiv="refresh" content="5; URL="http://projet-web-mahe.herokuapp.com/home"">
 </head>
 
 <body>
