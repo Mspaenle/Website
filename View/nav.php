ef2e2d68a4692cb035c7045d893ade841af3564b
Unknown <Mahe.spaenle@etu.umontpellier.fr>
2017-05-24 12:00:01 +0200

    Update htaccess

--- View/nav.php
+++ View/nav.php
@@ -11,7 +11,7 @@
       <ul class="nav navbar-nav">
         <li><a href="index.php">Home</a></li>
         <li><a href="games.php">Video Games</a></li>
-        <li><a href="ranking.php">Ranking</a></li>
+        <li><a href="ranking">Ranking</a></li>
         <li><a href="newParty.php">Start a tournament</a></li>
         <li><a href="current_party.php">Current party</a></li>
         <li><a href="admin.php">Add a game</a></li>
