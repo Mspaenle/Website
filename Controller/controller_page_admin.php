287f633fd8522aa117860e075c6c21a227ee1f0a
Unknown <Mahe.spaenle@etu.umontpellier.fr>
2017-05-24 13:48:30 +0200

    Admin redirection

--- Controller/controller_page_admin.php
+++ Controller/controller_page_admin.php
@@ -1,7 +1,7 @@
 <?php
 
   if(isset($_COOKIE['idadmin'])){
-    require_once("../Controller/controller_page_choice.php");
+    require_once("admin-page");
   }
   else {
     require_once("../View/admin.php");
