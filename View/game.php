bd313a0133d53b7f4e2b13b4dd4cdfe1b0bc1cf9
Unknown <Mahe.spaenle@etu.umontpellier.fr>
2017-05-26 15:13:23 +0200

    Corrected

--- View/game.php
+++ View/game.php
@@ -33,7 +33,7 @@
             <div class="caption">
               <h3><?php echo $donnees['namegame'];?></h3>
               <p><?php echo $donnees['description'];?></p>
-              <p><a href="../Controllet/controller_display_game.php?idGame=<?php echo $donnees['idgame'] ?>" class="btn btn-primary" role="button">More infos</a><a href="form-new-party-1" class="btn btn-default" role="button">Play this game</a></p>
+              <p><a href="../Controller/controller_display_game.php?idGame=<?php echo $donnees['idgame'] ?>" class="btn btn-primary" role="button">More infos</a><a href="form-new-party-1" class="btn btn-default" role="button">Play this game</a></p>
             </div>
           </div>
         </div>
